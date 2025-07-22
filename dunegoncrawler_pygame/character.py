1ec22804793aff167a84a306754ecba7cad0cfe3
TheGalaxyPizza <satvikp626@gmail.com>
2025-07-21 20:47:37 -0400

    I'm calling the progress on these videos days. So basically, I'm done with Day 6 and 7.

--- dunegoncrawler_pygame/character.py
+++ dunegoncrawler_pygame/character.py
@@ -3,13 +3,21 @@ import math
 import constants
 
 class Character:
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, animation_list):
         self.flip = False
-        self.image = image
+        self.animation_list = animation_list
+        self.frame_index = 0
+        self.action = 0#0 means idle, #1 means run
+        self.update_time = pygame.time.get_ticks()
+        self.running = False
+        self.image = animation_list[self.action][self.frame_index]
         self.rect = pygame.Rect(0, 0, 40, 40)
         self.rect.center = (x, y)
 
     def move(self, dx, dy):
+        self.running = False
+        if dx != 0 or dy != 0:
+            self.running = True
 
         if dx < 0:
             self.flip = True
@@ -22,6 +30,32 @@ class Character:
 
         self.rect.x += dx
         self.rect.y += dy
+
+
+    def update(self):
+        #check what action the player is performing
+        if self.running:
+            self.update_action(1)
+        else:
+            self.update_action(0)
+
+        animation_cooldown = 70
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+
+    def update_action(self, new_action):
+        #check if the new action is different to the previous one.
+        if new_action != self.action:
+            self.action = new_action
+        #update the animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
+
+
 
     def draw(self, surface):
         flipped_image = pygame.transform.flip(self.image, self.flip, False)
--- dunegoncrawler_pygame/main.py
+++ dunegoncrawler_pygame/main.py
@@ -23,14 +23,20 @@ def scale_img(image, scale):
     return pygame.transform.scale(image, (w * scale, h * scale))
 
 
-
-
-#show player (image)
-player_image = pygame.image.load("assets/images/characters/elf/idle/0.png").convert_alpha()
-player_image = scale_img(player_image, constants.SCALE)
+animation_types = ["idle", "run"]
+#load images
+animation_list = []
+for animation in animation_types:
+#reset temporary list of images:
+    temp_list = []
+    for i in range(4):
+        img = pygame.image.load(f"assets/images/characters/elf/{animation}/{i}.png").convert_alpha()
+        img = scale_img(img, constants.SCALE)
+        temp_list.append(img)
+    animation_list.append(temp_list)
 
 #create player
-player = Character(100, 100, player_image)
+player = Character(100, 100, animation_list)
 
 #main game loop
 run = True
@@ -56,6 +62,10 @@ while run:
 
     #move the player
     player.move(dx, dy)
+
+    #update player
+    player.update()
+
 
     #draw the player
     player.draw(screen)
